3502f039c51951e3b0727da8f4453aadceea0aa0
Erick Eduardo Mejia Mejia <erick.mejia@gruposatelite.net>
2023-03-18 06:24:20 -0600

    avance con las rutas

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+  termino:string = '';
+  hayError:boolean = false;
+
+  constructor( private paisService: PaisService){}
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino)
+    this.paisService.buscarPais(this.termino).subscribe((res) => {
+      console.log(res);
+    }, (err) => {
+        this.hayError = true;
+    });
+  }
+}
---
+++ src/app/pais/pais.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    VerPaisComponent,
+    PorRegionComponent,
+  ],
+  exports: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    VerPaisComponent,
+    PorRegionComponent,    
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ]
+})
+export class PaisModule { }
